babb6b8a83328aad60ef93df0999c93ee0f82564
Greg Lombardo <greg.lombardo@gfi.com>
2013-11-21 10:25:03 -0500

    updating gemfile lock for deployment testing

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :rails_env, 'staging'
 set :unicorn_env, 'staging'
 set :app_env, 'staging'
-set :domain, ''
+set :domain, '10.24.103.50'
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db, domain, primary: true
