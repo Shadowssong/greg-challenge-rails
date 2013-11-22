1634115a2c51c508d42df0f103cd00ba51a6f93c
Greg Lombardo <greg@sovrano-mbp.home>
2013-11-21 22:14:49 -0500

    updating readme

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :rails_env, 'production'
 set :unicorn_env, 'production'
 set :app_env, 'production'
-set :domain, '10.24.103.141'
+set :domain, '192.168.1.9'
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db, domain, primary: true
